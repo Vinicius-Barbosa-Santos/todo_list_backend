eb3dbbdfdda28e3f394e07e9dd190c284c2add3a
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-05-16 18:55:48 -0300

    feat: add route to created a new task

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ const TaskModel = require('./src/models/task.model')
 
 dotenv.config()
 const app = express()
+app.use(express.json())
+
 
 connectToDatabase()
 
 app.get('/tasks', async (request, response) => {
+    try {
+        const tasks = await TaskModel.find({})
+        response.status(200).send(tasks)
+    } catch (err) {
+        response.status(500).send(err)
+    }
+})
 
-    const tasks = await TaskModel.find({})
-    response.status(200).send(tasks)
+app.post('/tasks', async (request, response) => {
+    try {
+        const newTask = new TaskModel(request.body)
+
+        await newTask.save()
+
+        response.status(201).send(newTask)
+    } catch (err) {
+        response.status(500).send(err)
+    }
 })
 
 const PORT = 3000
